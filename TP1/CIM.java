1e5b852e762b1e409560b553d1743d3bbc470709
Selion11 <gdecaro@itba.edu.ar>
2024-08-09 23:03:27 -0300

    Fix square creation. Repeted and not going up

--- TP1/CIM.java
+++ TP1/CIM.java
@@ -8,20 +8,32 @@ public class CIM {
         int L = 20;
         particle[] particles = fileProcessor.readFile("TP1/dynamic_CIM_input.txt", "TP1/static_CIM_input.txt", 1.0F);
         int M = 25;
+        square[] squares = calculations(L,M);
 
-        calculations(L,M);
+        for(particle p : particles) {
+            int check = 0;
+            int s = 0;
+            while(check == 0){
+                check = squares[s].add_particle(p);
+                s++;
+            }
+        }
+        System.out.println("Check Finished");
     }
 
-    private static void calculations(int L, int M){
-        float start = 0.0F;
+    private static square[] calculations(int L, int M){
         float step = (float) L /M;
+        float start = 0.0F;
         float finish = step;
-        int counter = 1;
+        int counter = 0;
+        square[] squares = new square[M];
         while(finish <= 20){
-            System.out.println("Step " + counter + " Start: " + start + " Finish: " + finish);
+            square aux = new square(start,finish,start,finish,counter +1);
             start += step;
             finish += step;
+            squares[counter] = aux;
             counter++;
         }
+        return squares;
     }
 }
---
+++ TP1/square.java
@@ -0,0 +1,85 @@
+package TP1;
+
+public class square{
+    private float x_stop,y_stop,x_start,y_start;
+    private int id;
+    private int array_size = 5;
+    particle[] particles = new particle[array_size];
+    int particle_index = 0;
+
+    public square(float x_start, float x_stop, float y_start, float y_stop, int id){
+        this.x_stop = x_stop;
+        this.y_stop = y_stop;
+        this.x_start = x_start;
+        this.y_start = y_start;
+        this.id = id;
+    }
+
+    public int add_particle(particle p){
+        if(!checkParticle(p)){
+            return 0;
+        }
+        int retVal = 0;
+        while(retVal != 1){
+            if(particle_index < array_size){
+                particles[particle_index] = p;
+                particle_index++;
+                retVal = 1;
+            }else{
+                particles = expand_array();
+            }
+        }
+        return retVal;
+    }
+
+    private boolean checkParticle(particle p){
+        return (p.getX() <= x_stop && p.getY() <= y_stop);
+    }
+
+    private particle[] expand_array(){
+        array_size += 5;
+        particle[] aux = new particle[array_size];
+        System.arraycopy(particles, 0, aux, 0, particles.length);
+        return aux;
+    }
+
+    public void setX_stop(float x_stop) {
+        this.x_stop = x_stop;
+    }
+
+    public void setX_start(float x_start) {
+        this.x_start = x_start;
+    }
+
+    public void setY_start(float y_start) {
+        this.y_start = y_start;
+    }
+
+    public void setY_stop(float y_stop) {
+        this.y_stop = y_stop;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getX_stop() {
+        return x_stop;
+    }
+
+    public float getY_stop() {
+        return y_stop;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getX_start() {
+        return x_start;
+    }
+
+    public float getY_start() {
+        return y_start;
+    }
+}
