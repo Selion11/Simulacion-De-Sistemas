c4f253001dcaf043b974654bfef7483f830e0766
Selion11 <gdecaro@itba.edu.ar>
2024-10-07 10:39:45 -0300

    initial conditions

--- TP4/systesm1.java
+++ TP4/systesm1.java
@@ -1,4 +1,20 @@
 package TP4;
 
+import java.io.IOException;
+
 public class systesm1 {
+    /*Parametros:
+    * Masa = 70kg
+    * k = 10^4 N/m
+    * gamma = 100 kg/s
+    * tfinal = 5 segundos
+    *
+    * Condiciones iniciales:
+    * r(t = 0) = 1 m
+    * v(t = 0) = - A * gamma/2m
+    *
+    * r = A exp(-(gamma/2m)*t) * cos((k/m - gamma^2/4m^2)^0.5 *t)*/
+    public static void main(String[] args) throws IOException {
+        
+    }
 }
