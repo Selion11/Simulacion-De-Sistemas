58696b0059e03832332a742c45584921596943f1
Francisco Dominguez <fdominguez@itba.edu.ar>
2024-10-08 11:50:46 -0300

    Interfaz Algorithm

---
+++ TP4/algorithms/Algorithm.java
@@ -0,0 +1,18 @@
+package TP4.algorithms;
+
+public interface Algorithm {
+    public void runAlgorithm();
+    public double getR();
+
+    default double OscillatorForce(double r, double v, double m, double k, double gamma) {
+        return (-k*r -gamma*v)/m;
+    }
+
+    default double EulerPosition(double r, double v, double a, double dt) {
+        return r + dt*v + dt*dt*a/2;
+    }
+
+    default double EulerVelocity(double v, double a, double dt) {
+        return v + dt*a;
+    }
+}
