12958b211c194e7148bafdc577446b6215361ac6
Francisco Dominguez <fdominguez@itba.edu.ar>
2024-10-13 19:57:38 -0300

    For time vs y

---
+++ TP4/py/timevsy.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = "TP4/outputs/System2/100.0_10.0.csv"
+
+df = pd.read_csv(file, delimiter=';')
+
+# Access each column as a Pandas Series
+t = df['time'].tolist()
+y = df['a'].tolist()
+
+
+# Calcular el desplazamiento en el tiempo
+
+
+# Gráfico
+plt.figure(figsize=(8, 6))
+plt.plot(t, np.abs(y), 'b', label='|y(t)|', linewidth=2)
+
+# Etiquetas y título
+plt.xlabel('Tiempo (s)', fontsize=14)
+plt.ylabel('|y| (m)', fontsize=14)
+plt.title('Oscilador amortiguado', fontsize=16)
+
+# Parámetros en el gráfico
+plt.text(102, 0.5, f'k = {100} kg/s²\nω = {10} rad/s', fontsize=12, ha='left', va='center')
+
+# Mostrar el gráfico
+plt.grid(True)
+plt.show()
